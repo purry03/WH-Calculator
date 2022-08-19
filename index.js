d0eea1e65e68283a7fe8bf79b9e7f1bb052e596a
Your Name <you@example.com>
2022-08-19 09:19:46 +0530

    added industrials

--- index.js
+++ index.js
@@ -3,14 +3,21 @@ let massList = {
     'RollingBattleshipHot': 300000,
     'RollingBattleshipCold': 200000,
     'RollingHIC': 138000,
+    'RollingHauler': 122000,
     "Frigate": 1100,
     "Destroyer": 1700,
     "Cruiser": 13000,
+    "Hauler": 11000,
+    "DST": 20000,
+    "Porpoise": 4500,
+    "Orca": 150000,
     "Battlecruiser": 14250,
     "Battleship": 95000,
+    "Freighter": 900000,
     "Carrier": 1230000,
     "CarrierHot": 1730000,
-    "Dreadnought": 1260000
+    "Dreadnought": 1260000,
+    "Rorqual": 800000,
 }
 
 function getMass(ship) {
